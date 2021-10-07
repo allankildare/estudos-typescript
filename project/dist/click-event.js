a1766daf88ea2ee36cb04f165d01f191b4bfdb5d
allankildare <allankildare27@gmail.com>
2021-10-07 17:20:39 -0300

    feat: evento de clique via console

---
+++ project/dist/click-event.js
@@ -0,0 +1,6 @@
+"use strict";
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    console.log('Fui clicado :)');
+});
+//# sourceMappingURL=click-event.js.map
