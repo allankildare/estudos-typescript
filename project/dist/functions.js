688290cf5666cadeab5d3014cd6df8dd47a8005a
allankildare <allankildare27@gmail.com>
2021-10-07 17:32:24 -0300

    chore: indentacao melhorada e remocao de argumento desnecessario

--- project/dist/functions.js
+++ project/dist/functions.js
@@ -3,7 +3,7 @@ function add(n1, n2) {
     return n1 + n2;
 }
 function printResult(num) {
-    console.log('Result: ' + num);
+    console.log("Result: " + num);
 }
 function addAndHandle(n1, n2, cb) {
     var result = n1 + n2;
@@ -16,10 +16,10 @@ console.log(combinedValues(8, 8));
 addAndHandle(10, 20, function (result) {
     console.log(result);
 });
-function sendRequest(data, cb) {
-    return cb({ data: 'Hi there!' });
+function sendRequest(cb) {
+    return cb({ data: "Hi there!" });
 }
-sendRequest('Send this!', function (response) {
+sendRequest(function (response) {
     console.log(response);
     return true;
 });
--- project/src/functions.ts
+++ project/src/functions.ts
@@ -1,36 +1,36 @@
 //                                  : tipando o retorno da funcao
 function add(n1: number, n2: number): number {
-    return n1 + n2
+  return n1 + n2;
 }
 
 function printResult(num: number): void {
-    console.log('Result: ' + num)
+  console.log("Result: " + num);
 }
 
 function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2
-    cb(result)
+  const result = n1 + n2;
+  cb(result);
 }
 
-printResult(add(5,12))
+printResult(add(5, 12));
 
-let combinedValues: (a: number, b: number) => number
+let combinedValues: (a: number, b: number) => number;
 
-combinedValues = add
+combinedValues = add;
 // combinedValues = printResult
 
-console.log(combinedValues(8,8))
+console.log(combinedValues(8, 8));
 
 addAndHandle(10, 20, (result) => {
-    console.log(result)
-})
+  console.log(result);
+});
 
-function sendRequest(data: string, cb: (response: any) => void) {
-    // ... sending a request with "data"
-    return cb({data: 'Hi there!'});
-  }
-   
-  sendRequest('Send this!', (response) => { 
-    console.log(response);
-    return true;
-   });
+function sendRequest(cb: (response: any) => void) {
+  // ... sending a request with "data"
+  return cb({ data: "Hi there!" });
+}
+
+sendRequest((response) => {
+  console.log(response);
+  return true;
+});
