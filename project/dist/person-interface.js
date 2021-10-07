47b29bde8f383b15237c992f7edf4cc360ce5c39
allankildare <allankildare27@gmail.com>
2021-10-07 17:20:52 -0300

    feat: primeira interface

---
+++ project/dist/person-interface.js
@@ -0,0 +1,11 @@
+"use strict";
+var user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet: function (phrase) {
+        console.log(phrase + " " + this.name);
+    }
+};
+user1.greet('Olá, eu me chamo Allan');
+//# sourceMappingURL=person-interface.js.map
